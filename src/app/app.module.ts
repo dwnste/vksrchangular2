97e5bf55db52e3997443c3a3bd441131ec5869da
Sergey Makoveev <sergey@makoveev.info>
2017-06-19 10:39:27 +0300

    app.module: форматирование

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { YaCoreModule }  from 'angular2-yandex-maps';
+import { YaCoreModule } from 'angular2-yandex-maps';
 
 @NgModule({
   declarations: [
